0f7f05294de35cef893c3a549a37905fe68b8f60
redbaland <misterxs5001@gmail.com>
2024-05-08 20:58:06 +0300

    Old dotfiles for hyprland

---
+++ .config/hypr/xdg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# __  ______   ____ 
+# \ \/ /  _ \ / ___|
+#  \  /| | | | |  _ 
+#  /  \| |_| | |_| |
+# /_/\_\____/ \____|
+#                   
+# ----------------------------------------------------- 
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
+killall xdg-desktop-portal
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
